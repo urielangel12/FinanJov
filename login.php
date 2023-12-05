7b64968ec50b7f9e50fba5a2d22c6ce8df928100
urielangel12 <abriceno551@gmail.com>
2023-12-04 23:36:22 -0500

    cambios 2

--- login.php
+++ login.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="login css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-...tu-integridad-aquí..." crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.15.0/font/bootstrap-icons.css" integrity="sha512-...tu-integridad-aquí..." crossorigin="anonymous" />
     <!-- Agrega estas líneas en la sección <head> de tu HTML -->
 <script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js"></script>
@@ -35,7 +37,7 @@
 
                     </a>
                     <a href="" class="social-icon">
-                        <i class="fab fa-twiter"> </i>
+                        <i class="fab fa-discord"> </i>
                     </a>
                     <a href="" class="social-icon">
                         <i class="fab fa-google"> </i>
@@ -71,7 +73,7 @@
 
                     </a>
                     <a href="#" class="social-icon">
-                        <i class="fab fa-twiter"> </i>
+                        <i class="fab fa-discord"> </i>
                     </a>
                     <a href="#" class="social-icon">
                         <i class="fab fa-google"> </i>
@@ -98,7 +100,7 @@
 
             <div class="panel right-panel">
                 <div class="content">
-                    <h3>no eres miembro de Finanjov?</h3>
+                    <h3>No eres miembro de Finanjov?</h3>
                     <p>
                         "En Finanjov, tu viaje financiero comienza con un simple paso: la creación de tu cuenta.
                         Regístrate ahora y abre las puertas a préstamos, inversiones y asesoramientos que te acercarán a
--- prestamos.php
+++ prestamos.php
@@ -1,36 +1,130 @@
-
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="cssprestamos.css/prestamos.css">
-  <title>Calculadora de Tasa de Interés</title>
-</head>
-<body>
-  <div class="calculator">
-    <h2>Calculadora de Tasa de Interés</h2>
-    <form id="interestForm">
-      <label for="principal">Monto Principal:</label>
-      <input type="number" id="principal" required>
-
-      <label for="months">Plazo en meses:</label>
-      <input type="number" id="months" required>
-
-      <label for="interestRate">Tasa de Interés:</label>
-      <select id="interestRate">
-        <option value="0.10">10%</option>
-        <option value="0.40">40%</option>
-        <option value="0.60">60%</option>
-        <option value="0.80">80%</option>
-      </select> 
-
-      <button type="button" onclick="calcularInteres()">Calcular</button>
-    </form>
-
-    <div id="result"></div>
-  </div>
-
+  <link rel="stylesheet" href="cssprestamos/prestamos.css">
+  <link rel="stylesheet" href="cssprestamos/redes.css">
+  <link rel="stylesheet" href="cssprestamos/whasapth.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="cssprestamos/cerrar.css">
+  <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
   <script src="js/prestamos.js"></script>
+  <title>Prestamos</title>
+</head>
+
+<body>
+
+  <header>
+    
+    <div>
+      <div class="profile">
+        <h4>Usuario</h4>
+        <button onclick="window.location.href='salir.php'" class="btn" type="button">
+        <strong>Cerrar Sesion</strong>
+        <div id="container-stars">
+        <div id="stars"></div>
+        </div>
+
+  <div id="glow">
+    <div class="circle"></div>
+    <div class="circle"></div>
+  </div>
+</button>
+      </div>
+      
+    </div>
+  </header>
+<div class="contenido">
+  <div class="anuncio1">
+    
+  </div>
+  <div class="page-wrapper">
+    <h1 class="page-title">BIENVENIDO A FINANJOV
+
+    </h1>
+    <br>
+    <div class="calculator">
+      <h2>Calculadora de Tasa de Interés</h2>
+
+      <form id="interestForm" class="form-style">
+        <label for="principal">Monto Principal:</label>
+        <input type="number" id="principal" required class="input-style">
+
+        <label for="months">Plazo en meses:</label>
+        <input type="number" id="months" required class="input-style">
+
+        <label for="interestRate">Tasa de Interés:</label>
+        <select id="interestRate" class="input-style">
+          <option value="0.10">10%</option>
+          <option value="0.40">40%</option>
+          <option value="0.60">60%</option>
+          <option value="0.80">80%</option>
+        </select>
+
+        <button type="button" onclick="calcularInteres()" class="button-style">Calcular</button>
+      </form>
+
+      <div id="result"></div>
+    </div> <br>
+
+    <div class="information-table">
+      <table>
+        <tr>
+        <td>
+        <h2>BENEFICIOS A NUESTROS USUARIOS</h2>
+
+        <div class="section">
+          <h3>Tasas Utepinas</h3>
+          <p>En la Universidad Tecnológica del Perú, nos enorgullece ofrecer tasas que se adaptan a las necesidades de nuestros estimados
+            alumnos y dedicados docentes. Creemos en brindar oportunidades accesibles para que la educación de calidad esté al alcance de todos.
+            Además de nuestras tasas competitivas, también nos comprometemos a ir más allá de la enseñanza en el aula. Contamos con programas
+            especiales que ofrecen asesoramientos y charlas exclusivas para nuestros nuevos clientes.
+          </p>
+          <img src="img/charlas.jpg" alt="Imagen 1">
+        </div>
+
+        <div class="section">
+          <h3>Charlas</h3>
+          <p>Nuestras charlas y asesoramientos están diseñados para abordar temas fundamentales que van más allá del aula y que son esenciales
+            para el éxito personal y profesional de nuestros alumnos. Algunos de los temas que cubrimos incluyen:<br>
+            1. Desarrollo de habilidades blandas<br>
+            2. Planificación de carrera<br>
+            3. Emprendimiento y liderazgo <br>
+            4. Adaptabilidad y resilencia <br>
+            5. Ética Profesional
+          </p>
+          <img src="img/habilidades.webp" alt="Imagen 2">
+        </div>
+        </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div class="anuncio2">
+    
+  </div>
+</div>
+
+    <div class="contact-bar">
+      <h2>Redes Sociales</h2>
+      <p>Unete a la Familia FinanJov siguiendonos:</p>
+      <div class="radio-inputs">
+            <section class="buttons">
+                <a href="https://www.facebook.com/macarius2.0" class="fa fa-facebook"></a>
+                <a href="https://twitter.com/dota2ti" class="fa fa-twitter"></a>
+                <a href="https://accounts.google.com/AccountChooser/signinchooser?service=mail&continue=
+                   https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser&ec=
+                   asw-gmail-globalnav-signin&theme=glif" class="fa fa-google-plus"></a>
+                <a href="https://www.youtube.com/@redbarrelsgames" class="fa fa-youtube"></a>
+                <a href="https://www.linkedin.com/jobs/?src=direct%2Fnone&veh=direct%2Fnone%7Cdirect%2Fnone" class="fa fa-linkedin"></a>
+              </section>
+            <a href="https://api.whatsapp.com/send?phone=+51937665575&text=Hola, quiero iniciar un prestamo con ustedes" class="btn-wsp" target="_blank">
+                <i class="fa fa-whatsapp icono"></i>
+            </a>
+      </div>
+    </div>  
 </body>
+
 </html>
